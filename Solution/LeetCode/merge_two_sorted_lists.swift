670edfdf1deb7ab1626195054c849af4dce9f958
bokyungseo <maybutter756@gmail.com>
2023-02-09 17:38:17 +0900

    refactor: LeetCode 21 풀이 수정

--- Solution/LeetCode/merge_two_sorted_lists.swift
+++ Solution/LeetCode/merge_two_sorted_lists.swift
@@ -10,6 +10,7 @@ import Foundation
 // MARK: - 67. Merge Two Sorted Lists
 // https://leetcode.com/problems/merge-two-sorted-lists/description/
 
+// 각 링크드 리스트 순회와 동시에 정렬할 수 있으면 좋을 것 같음
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
         var current1 = list1
@@ -17,17 +18,23 @@ class Solution {
 
         var list: [Int] = []
 
-        while current1 != nil {
-            list.append(current1!.val)
-            current1 = current1?.next
+        while current1 != nil || current2 != nil {
+            if current1 == nil {
+                list.append(current2!.val)
+                current2 = current2?.next
+            } else if current2 == nil {
+                list.append(current1!.val)
+                current1 = current1?.next
+            } else {
+                if current1!.val < current2!.val {
+                    list.append(current1!.val)
+                    current1 = current1?.next
+                } else {
+                    list.append(current2!.val)
+                    current2 = current2?.next
+                }
+            }
         }
-
-        while current2 != nil {
-            list.append(current2!.val)
-            current2 = current2?.next
-        }
-
-        list.sort()
 
         if list.count == 0 { return nil }
 
